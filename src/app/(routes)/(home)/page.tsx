499e5915d85d0c35277bc05763b5c847ef0bb3db
Alex Naltu <naltuok@gmail.com>
2024-08-27 08:30:46 +0200

    starting the UI build

---
+++ src/app/(routes)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import UserNavbar from "@/app/components/navbar/user-navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <UserNavbar />
+    </div>
+  );
+}
---
+++ src/app/components/navbar/user-navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserNavbar = () => {
+  return (
+    <div className="flex justify-end">
+      <h1>Sign In</h1>
+    </div>
+  );
+};
+
+export default UserNavbar;
