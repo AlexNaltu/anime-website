e3ccf569ee5a27e316361c391bee804736de0f63
Alex Naltu <naltuok@gmail.com>
2024-09-06 11:38:31 +0200

    UI change to the newsletter

--- src/components/newsletter/newsletter.tsx
+++ src/components/newsletter/newsletter.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
 import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { TbArrowBigRightFilled } from "react-icons/tb";
 
 const Newsletter = () => {
   return (
-    <div>
+    <div className="m-2 max-w-[600px] mx-auto md:px-2 md:mx-0">
       <Image
         src={"/newsletter_images/newsletter-girl.png"}
         alt="/"
-        width={300}
-        height={300}
+        width={500}
+        height={500}
       />
+      <form className="flex flex-col gap-2 xs:flex-row">
+        <Input
+          placeholder="Your Email"
+          className="border-black border-2 bg-[#e94d2c] placeholder:text-white placeholder:font-black text-white"
+        />
+        <Button type="submit" className="px-8 bg-black rounded-lg w-fit">
+          <TbArrowBigRightFilled size={25} />
+        </Button>
+      </form>
     </div>
   );
 };
