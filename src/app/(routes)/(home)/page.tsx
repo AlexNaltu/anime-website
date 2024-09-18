f5744f0920070f7334efed76bae805a0077c63fd
Alex Naltu <naltuok@gmail.com>
2024-09-18 04:55:47 +0200

    small code changes and test posts not working

--- src/app/(routes)/(home)/page.tsx
+++ src/app/(routes)/(home)/page.tsx
@@ -3,6 +3,7 @@ import Filters from "@/components/filters/filters";
 import Newsletter from "@/components/newsletter/newsletter";
 import LatestNews from "@/components/posts/latest-news";
 import Posts from "@/components/posts/posts";
+import TestPosts from "@/components/posts/test-posts";
 import { getQueryClient } from "@/lib/query";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
@@ -23,6 +24,7 @@ export default async function Home() {
       <Filters />
       <div className="md:flex justify-between max-w-[1300px] mx-auto">
         <HydrationBoundary state={dehydrate(queryClient)}>
+          <TestPosts />
           <Posts />
         </HydrationBoundary>
         <div className="flex flex-col my-5 mx-2">
