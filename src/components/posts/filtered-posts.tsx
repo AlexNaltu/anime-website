88b27b65822e7e87884f3ce527812eb35a951dfb
Alex Naltu <naltuok@gmail.com>
2024-09-19 09:55:06 +0200

    added pagination to the filtered posts

--- src/components/posts/filtered-posts.tsx
+++ src/components/posts/filtered-posts.tsx
@@ -8,8 +8,16 @@ import React, { useMemo, useState } from "react";
 import { formatDate } from "date-fns";
 import { Button } from "../ui/button";
 import { useSearchParams } from "next/navigation";
+import ReactPaginate from "react-paginate";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
+interface PageClickData {
+  selectedPage: number;
+  selected: number;
+}
 
 const FilteredPosts = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   const params = useSearchParams();
   const category = params.get("category") as ICategory | null;
   const { data, error, isLoading } = useQuery({
@@ -21,6 +29,12 @@ const FilteredPosts = () => {
     },
   });
 
+  const handlePageClick = (selectedPage: PageClickData) => {
+    setCurrentPage(selectedPage.selected);
+  };
+  const itemsPerPage = 2;
+  const offset = currentPage * itemsPerPage;
+
   const categoryFilterPosts = useMemo(() => {
     if (category && data) {
       return data.filter((post: IPost) =>
@@ -31,27 +45,34 @@ const FilteredPosts = () => {
     return data;
   }, [data, category]);
 
-  return (
-    <div>
-      {categoryFilterPosts.map((post: IPost) => (
-        <Link key={post._id} href={`/posts/${post.slug}`}>
-          <Image
-            src={post.mainImage}
-            alt={post.title}
-            width={300}
-            height={200}
-          />
-          <h2>{post.title}</h2>
-          <p>{post.description}</p>
-          {post.category.map((cat: ICategory) => (
-            <span key={cat.title} className="bg-red-950 text-white">
-              {cat.title}
-            </span>
+  const paginatedPosts = categoryFilterPosts
+    ? categoryFilterPosts.slice(offset, offset + itemsPerPage)
+    : data?.slice(offset, offset + itemsPerPage);
+
+  if (error) return <div>{error.message}</div>;
+  if (categoryFilterPosts && data)
+    return (
+      <div>
+        <div>
+          {paginatedPosts.map((post: IPost) => (
+            <div key={post._id}>
+              <h1>{post.title}</h1>
+            </div>
           ))}
-        </Link>
-      ))}
-    </div>
-  );
+        </div>
+        <ReactPaginate
+          pageCount={Math.ceil(paginatedPosts.length / itemsPerPage)}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          activeClassName={"active"}
+          nextLabel={<MdKeyboardArrowRight size={25} />}
+          previousLabel={<MdKeyboardArrowLeft size={25} />}
+          breakLabel={"..."}
+        />
+      </div>
+    );
 };
 
 export default FilteredPosts;
