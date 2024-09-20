4167d021455b93e2bf76ffe2e25876b411fc5be3
Alex Naltu <naltuok@gmail.com>
2024-09-20 06:33:12 +0200

    removed filteredPosts variable

--- src/components/posts/filtered-posts.tsx
+++ src/components/posts/filtered-posts.tsx
@@ -52,41 +52,41 @@ const FilteredPosts = () => {
     return data;
   }, [data, category]);
 
-  const paginatedPosts = categoryFilterPosts
-    ? categoryFilterPosts.slice(offset, offset + itemsPerPage)
-    : data?.slice(offset, offset + itemsPerPage);
-
   if (error) return <div>{error.message}</div>;
   if (categoryFilterPosts && data)
     return (
       <div className="px-2 sm:px-4 max-w-[1300px] mx-auto">
         <div>
-          {paginatedPosts.map((post: IPost) => (
-            <Card
-              className="rounded-none sm:flex justify-between"
-              key={post._id}
-            >
-              <CardHeader className="p-2 relative w-full sm:min-w-[40%] min-[900px]:w-[400px] h-[200px]">
-                <Image
-                  src={post.mainImage}
-                  alt={post.title}
-                  fill
-                  sizes="(max-width: 768px) (height: 200px)"
-                  style={{ objectFit: "cover" }}
-                />
-              </CardHeader>
-              <CardContent className="p-2 flex flex-col self-end">
-                <CardTitle>{post.title}</CardTitle>
-                <div className="flex justify-between text-xs my-1">
-                  <p>{post.readingTime} min read</p>
-                  <p>{formatDate(new Date(post.publishedAt), "dd.MM.yyyy")}</p>
-                </div>
-                <CardDescription className="line-clamp-4 sm:line-clamp-3">
-                  {post.description}
-                </CardDescription>
-              </CardContent>
-            </Card>
-          ))}
+          {categoryFilterPosts
+            .slice(offset, offset + itemsPerPage)
+            .map((post: IPost) => (
+              <Card
+                className="rounded-none sm:flex justify-between"
+                key={post._id}
+              >
+                <CardHeader className="p-2 relative w-full sm:min-w-[40%] min-[900px]:w-[400px] h-[200px]">
+                  <Image
+                    src={post.mainImage}
+                    alt={post.title}
+                    fill
+                    sizes="(max-width: 768px) (height: 200px)"
+                    style={{ objectFit: "cover" }}
+                  />
+                </CardHeader>
+                <CardContent className="p-2 flex flex-col self-end">
+                  <CardTitle>{post.title}</CardTitle>
+                  <div className="flex justify-between text-xs my-1">
+                    <p>{post.readingTime} min read</p>
+                    <p>
+                      {formatDate(new Date(post.publishedAt), "dd.MM.yyyy")}
+                    </p>
+                  </div>
+                  <CardDescription className="line-clamp-4 sm:line-clamp-3">
+                    {post.description}
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            ))}
         </div>
         <ReactPaginate
           pageCount={Math.ceil(
