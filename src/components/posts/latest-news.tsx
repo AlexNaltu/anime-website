625fd0d1eaae99a5eb4fd963de773ef1083fc678
Alex Naltu <naltuok@gmail.com>
2024-09-22 09:43:01 +0200

    added links to the latest news

--- src/components/posts/latest-news.tsx
+++ src/components/posts/latest-news.tsx
@@ -1,6 +1,7 @@
 import { getLatestNews } from "@/actions/actions";
 import { IPost } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 import React from "react";
 
 const LatestNews = async () => {
@@ -10,9 +11,11 @@ const LatestNews = async () => {
       <h1 className="font-black text-xl mb-2">Latest News</h1>
       <div className="flex flex-col gap-3">
         {latestNews.map((post: IPost) => (
-          <div key={post._id}>
-            <h1>{post.title}</h1>
-          </div>
+          <Link href={`/posts/${post.slug}`} key={post._id}>
+            <h1 className="bg-transparent p-0  hover:bg-transparent hover:text-slate-300 w-fit">
+              {post.title}
+            </h1>
+          </Link>
         ))}
       </div>
     </div>
