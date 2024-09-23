50f9f6c1a179014dd0dd31f05c13757760d18070
Alex Naltu <naltuok@gmail.com>
2024-09-23 08:13:19 +0200

    added success page for the contact us form

---
+++ src/app/(routes)/(home)/contact/success/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SuccessPage = () => {
+  return (
+    <div className="max-w-[1300px] mx-auto px-2 sm:px-4">
+      <h1 className="my-4 font-black text-2xl lg:my-8">
+        Your message was sent successfully! We will get back to you as soon as
+        possible.
+      </h1>
+    </div>
+  );
+};
+
+export default SuccessPage;
