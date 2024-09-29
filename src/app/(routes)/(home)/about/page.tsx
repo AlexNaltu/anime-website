b68710c901a989807d61e63fbbd401bfe0adf22c
Alex Naltu <naltuok@gmail.com>
2024-09-29 07:33:10 +0200

    minor text bug

--- src/app/(routes)/(home)/about/page.tsx
+++ src/app/(routes)/(home)/about/page.tsx
@@ -10,14 +10,15 @@ const AboutPage = () => {
       <p className="sm:text-lg lg:text-xl">
         This site is created by a passionate anime fan, run entirely by me. My
         goal is to provide a space where fellow anime enthusiasts can explore
-        and discuss all things anime. Whether you're into the latest series,
-        classic favorites, or deep-dive discussions, you're in the right place!
+        and discuss all things anime. Whether you&apos;re into the latest
+        series, classic favorites, or deep-dive discussions, you&apos;re in the
+        right place!
         <br />
         <br />I manage everything here solo, from writing content to keeping the
-        site updated with fresh recommendations and insights. If you're a fan of
-        anime and want a cozy corner to indulge in anime talk, you've found your
-        spot. Thanks for visiting, and I hope you enjoy the content as much as I
-        enjoy creating it!
+        site updated with fresh recommendations and insights. If you&apos;re a
+        fan of anime and want a cozy corner to indulge in anime talk,
+        you&apos;ve found your spot. Thanks for visiting, and I hope you enjoy
+        the content as much as I enjoy creating it!
       </p>
       <h2 className="text-xl font-black my-4">You can support me here:</h2>
       <div className="flex flex-col gap-2">
