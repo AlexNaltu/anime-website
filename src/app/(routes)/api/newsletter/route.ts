be0e6d994fdddbe7190be80a4b7d2ef08c861784
Alex Naltu <naltuok@gmail.com>
2024-09-29 07:38:32 +0200

    bug fix for the newsletter route

--- src/app/(routes)/api/newsletter/route.ts
+++ src/app/(routes)/api/newsletter/route.ts
@@ -1,26 +1,22 @@
 import { NextResponse } from "next/server";
 
-export default async function POST(req: Request) {
-  // get the email from the request body
+export async function POST(req: Request) {
   const { email } = await req.json();
 
-  // check if the email is provided
   if (!email) {
     return NextResponse.json({ error: "Email is required" }, { status: 400 });
   }
 
-  // get the mailchimp api key, server and audience from the environment variables
-  const MailchimlKey = process.env.MAILCHIMP_API_KEY;
-  const MailchimpServer = process.env.MAILCHIMP_SERVER;
-  const MailchimpAudience = process.env.MAILCHIMP_AUDIENCE;
+  const MailchimpKey = process.env.MAILCHIMP_API_KEY;
+  const MailchimpServer = process.env.MAILCHIMP_API_SERVER;
+  const MailchimpAudience = process.env.MAILCHIMP_AUDIENCE_ID;
 
   const customUrl = `https://${MailchimpServer}.api.mailchimp.com/3.0/lists/${MailchimpAudience}/members`;
 
-  // make a request to the mailchimp api to subscribe the user
   const response = await fetch(customUrl, {
     method: "POST",
     headers: {
-      Authorization: `apikey ${MailchimlKey}`,
+      Authorization: `apikey ${MailchimpKey}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
